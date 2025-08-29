1f38678e12c8bd125789393a01f6449c2229593b
Sivaraj R <sivaraj@ti.com>
2025-08-30 00:55:06 +0530

    release: 11_00_00_17
    Signed-off-by: Sivaraj R <sivaraj@ti.com>

--- mcal_config/plugins/AM263Px/Mcu_TI_AM263Px/generate/include/Mcu_Cfg.h
+++ mcal_config/plugins/AM263Px/Mcu_TI_AM263Px/generate/include/Mcu_Cfg.h
@@ -117,6 +117,9 @@ enum
 #define MCU_PWM_ENABLE [!IF "(node:exists(as:modconf('Pwm')[1]/PwmGeneral)) or (node:exists(as:modconf('Cdd_Pwm')[1]/CddPwmGeneral))"!](STD_ON)[!ELSE!](STD_OFF)[!ENDIF!]
 /** \brief Enable/Disable MCU ADC Enable */
 #define MCU_ADC_ENABLE [!IF "(node:exists(as:modconf('Adc')[1]/AdcGeneral))"!](STD_ON)[!ELSE!](STD_OFF)[!ENDIF!]
+/** \brief Enable/Disable MCU ETH Enable */
+#define MCU_ETH_ENABLE [!IF "(node:exists(as:modconf('Eth')[1]/EthGeneral))"!](STD_ON)[!ELSE!](STD_OFF)[!ENDIF!]
+/* @} */
 
 /*
 Reset Reason Config
@@ -283,6 +286,13 @@ Reset Reason Config
 [!VAR "AdcUnits" = "$AdcUnits+1"!][!//
 [!ENDLOOP!][!//
 #define MCU_ADC_HWUNIT ([!"num:i($AdcUnits)"!]U)
+
+/* MCU ETH Port */
+[!VAR "EthPorts" = "0"!][!//
+[!LOOP "as:modconf('Mcu')[1]/McuModuleConfiguration/McuEthConfiguration/*"!][!//
+[!VAR "EthPorts" = "$EthPorts+1"!][!//
+[!ENDLOOP!][!//
+#define MCU_ETH_PORTS ([!"num:i($EthPorts)"!]U)
 
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
@@ -487,7 +497,46 @@ typedef enum
     /** \brief Clocksource max */
     MCU_CLKSRC_MAX
 }Mcu_ClkSourceIdType;
-
+[!IF "(node:exists(as:modconf('Eth')[1]/EthGeneral))"!]
+/**
+ *  \brief  Type/Speed/Duplex Connection Config Options
+ *
+ *  Connection Config Options
+ *
+ */
+typedef enum
+{
+    MCU_ETH_MAC_CONN_TYPE_MII_10_HALF = 0x00U,
+    /**< MAC connection type for half-duplex 10Mbps MII mode */
+    MCU_ETH_MAC_CONN_TYPE_MII_10_FULL = 0x01U,
+    /**< MAC connection type for full-duplex 10Mbps MII mode */
+    MCU_ETH_MAC_CONN_TYPE_MII_100_HALF = 0x02U,
+    /**< MAC connection type for half-duplex 100Mbps MII mode */
+    MCU_ETH_MAC_CONN_TYPE_MII_100_FULL = 0x03U,
+    /**< MAC connection type for full-duplex 100Mbps MII mode */
+    MCU_ETH_MAC_CONN_TYPE_RMII_10_HALF = 0x04U,
+    /**< MAC connection type for half-duplex 10Mbps RMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RMII_10_FULL = 0x05U,
+    /**< MAC connection type for full-duplex 10Mbps RMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RMII_100_HALF = 0x06U,
+    /**< MAC connection type for half-duplex 100Mbps RMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RMII_100_FULL = 0x07U,
+    /**< MAC connection type for full-duplex 100Mbps RMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RGMII_FORCE_10_HALF = 0x08U,
+    /**< MAC connection type for forced half-duplex 10Mbps RGMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RGMII_FORCE_10_FULL = 0x09U,
+    /**< MAC connection type for forced full-duplex 10Mbps RGMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RGMII_FORCE_100_HALF = 0x0AU,
+    /**< MAC connection type for forced half-duplex 100Mbps RGMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RGMII_FORCE_100_FULL = 0x0BU,
+    /**< MAC connection type for forced full-duplex 100Mbps RGMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RGMII_FORCE_1000 = 0x0CU,
+    /**< MAC connection type for forced 1000Mbps RGMII mode */
+    MCU_ETH_MAC_CONN_TYPE_RGMII_DETECT_INBAND = 0x0DU,
+    /**< MAC connection type for RGMII inband detection mode (speed determined
+     *   based on received RGMII Rx clock) */
+} Mcu_MacConnectionType;
+[!ENDIF!]
 /* Requirements: SWS_Mcu_00030 */
 /**
  *  \brief Structure for data pre-setting to be initialized
@@ -606,7 +655,22 @@ typedef struct
     uint8 Mcu_AdcHWUniId;
 
 } Mcu_AdcConfigType;
-
+[!IF "(node:exists(as:modconf('Eth')[1]/EthGeneral))"!]
+/**
+ *  \brief Structure for Eth CPSW Control configuration
+ */
+typedef struct
+{
+    /** \brief ETH MAC port number */
+    uint8 macNum;
+    /** \brief ETH MII Clock output disable */
+    uint8 rmiiClkOutDisable;
+    /** \brief ETH enable ID mode */
+    uint8 idModeEnable;
+    /** \brief ETH MII MAC connection type */
+    Mcu_MacConnectionType macConnectionType;
+} Mcu_EthConfigType;
+[!ENDIF!]
 /**
  *  \brief Structure for PRCM configuration
  */
@@ -649,7 +713,13 @@ Mcu_PwmConfigPtrType;
  */
 typedef P2CONST (Mcu_AdcConfigType, AUTOMATIC, MCU_PBCFG)
 Mcu_AdcConfigPtrType;
-
+[!IF "(node:exists(as:modconf('Eth')[1]/EthGeneral))"!]
+/**
+ *  \brief Pointer to Eth CPSW Control configuration
+ */
+typedef P2CONST (Mcu_EthConfigType, AUTOMATIC, MCU_PBCFG)
+Mcu_EthConfigPtrType;
+[!ENDIF!]
 /**
  *  \brief MCU CONFIG Register READBACK structure
  */
@@ -691,6 +761,12 @@ typedef struct Mcu_ConfigType_s
     /** \brief ADC Configuration */
 	Mcu_AdcConfigPtrType     Mcu_AdcConfig;
 	#endif
+[!IF "(node:exists(as:modconf('Eth')[1]/EthGeneral))"!]	
+    #if (STD_ON == MCU_ETH_ENABLE)
+    /** \brief ETH Configuration */
+    Mcu_EthConfigPtrType     Mcu_EthConfig;
+    #endif
+[!ENDIF!]	
 } Mcu_ConfigType;
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
@@ -702,7 +778,13 @@ typedef struct Mcu_ConfigType_s
 #define MCU_TOP_CTRL_PARTITION0                                (2)
 #define MCU_CONTROLSS_CTRL_PARTITION0                          (3)
 /*Clock and reset MMRs partition*/
-#define MCU_TOP_RCM_PARTITION0                                 (5)
+#define MCU_TOP_RCM_PARTITION0                                 (5U)
+
+/* MSS CTRL partition */
+#define MCU_MSS_CTRL_PARTITION0                                (6U)
+
+/* MSS CTRL base address */
+#define MCU_CSL_MSS_CTRL_BASE   (0x50D00000U)
 
 #define MCU_CSL_CONTROLSS_CTRL_EPWM_STATICXBAR_SEL0                          (0x00000004U)
 
@@ -721,6 +803,8 @@ typedef struct Mcu_ConfigType_s
 #define MCU_CSL_TOP_CTRL_LOCK0_KICK1                                          (0x0000100CU)
 #define MCU_CSL_TOP_RCM_LOCK0_KICK0                                           (0x00001008U)
 #define MCU_CSL_TOP_RCM_LOCK0_KICK1                                           (0x0000100CU)
+#define MCU_CSL_MSS_CTRL_LOCK0_KICK0                                          (0x00001008U)
+#define MCU_CSL_MSS_CTRL_LOCK0_KICK1                                          (0x0000100CU)
 
 /* define the unlock and lock values for MSS_CTRL, TOP_CTRL, MSS_RCM, TOP_RCM*/
 #define MCU_TEST_KICK_LOCK_VAL                           (0x00000000U)
